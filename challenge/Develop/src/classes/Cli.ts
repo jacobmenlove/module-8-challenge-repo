1c243ec6c14d4edc121456efcd6d60c625e946e0
Jacob Menlove <jacobmenlove1@gmail.com>
2024-08-05 22:02:54 -0400

    13th push. frusterated.

--- challenge/Develop/src/classes/Cli.ts
+++ challenge/Develop/src/classes/Cli.ts
@@ -1,95 +1,98 @@
 
+import truck from './Truck';
+import motorbike from './Motorbike';
+
 import inquirer from 'inquirer';
 import { Car, Truck, Motorbike } from './Vehicle';
 
 export class Cli {
     private vehicles: (Car | Truck | Motorbike)[] = [];
     private selectedVehicleVin: string | null = null;
-
+  
     public addVehicle(vehicle: Car | Truck | Motorbike) {
-        this.vehicles.push(vehicle);
+      this.vehicles.push(vehicle);
     }
-
+  
     public async selectVehicle() {
-        const vehicleChoices = this.vehicles.map(vehicle => ({
-            name: `${vehicle.vin} -- ${vehicle.make} ${vehicle.model}`,
-            value: vehicle.vin,
-        }));
-
-        const answers = await inquirer.prompt([
-            {
-                type: 'list',
-                name: 'selectedVehicle',
-                message: 'Select a vehicle:',
-                choices: vehicleChoices,
-            }
-        ]);
-
-        this.selectedVehicleVin = answers.selectedVehicle;
-        this.performActions();
+      const vehicleChoices = this.vehicles.map(vehicle => ({
+        name: `${vehicle.vin} -- ${vehicle.make} ${vehicle.model}`,
+        value: vehicle.vin,
+      }));
+  
+      const answers = await inquirer.prompt([
+        {
+          type: 'list',
+          name: 'selectedVehicle',
+          message: 'Select a vehicle:',
+          choices: vehicleChoices,
+        }
+      ]);
+  
+      this.selectedVehicleVin = answers.selectedVehicle;
+      this.performActions();
     }
-
+  
     private async performActions() {
-        if (!this.selectedVehicleVin) return;
-
-        const selectedVehicle = this.vehicles.find(vehicle => vehicle.vin === this.selectedVehicleVin);
-
-        if (!selectedVehicle) return;
-
-        const actions = [
-            'Print Details',
-            'Start',
-            'Accelerate',
-            'Decelerate',
-            'Stop',
-            'Turn Right',
-            'Turn Left',
-            'Reverse',
-        ];
-
-        if (selectedVehicle instanceof Motorbike) {
-            actions.push('Wheelie');
+      if (!this.selectedVehicleVin) return;
+  
+      const selectedVehicle = this.vehicles.find(vehicle => vehicle.vin === this.selectedVehicleVin);
+  
+      if (!selectedVehicle) return;
+  
+      const actions = [
+        'Print Details',
+        'Start',
+        'Accelerate',
+        'Decelerate',
+        'Stop',
+        'Turn Right',
+        'Turn Left',
+        'Reverse',
+      ];
+  
+      if (selectedVehicle instanceof Motorbike) {
+        actions.push('Wheelie');
+      }
+  
+      const answers = await inquirer.prompt([
+        {
+          type: 'list',
+          name: 'action',
+          message: 'What would you like to do?',
+          choices: actions,
         }
-
-        const answers = await inquirer.prompt([
-            {
-                type: 'list',
-                name: 'action',
-                message: 'What would you like to do?',
-                choices: actions,
-            }
-        ]);
-
-        switch (answers.action) {
-            case 'Print Details':
-                selectedVehicle.printDetails();
-                break;
-            case 'Start':
-                (selectedVehicle as any).start();
-                break;
-            case 'Accelerate':
-                (selectedVehicle as any).accelerate(5);
-                break;
-            case 'Decelerate':
-                (selectedVehicle as any).decelerate(5);
-                break;
-            case 'Stop':
-                (selectedVehicle as any).stop();
-                break;
-            case 'Turn Right':
-                (selectedVehicle as any).turn('right');
-                break;
-            case 'Turn Left':
-                (selectedVehicle as any).turn('left');
-                break;
-            case 'Reverse':
-                (selectedVehicle as any).reverse();
-                break;
-            case 'Wheelie':
-                (selectedVehicle as Motorbike).wheelie();
-                break;
-        }
-
-        this.selectVehicle();
+      ]);
+  
+      switch (answers.action) {
+        case 'Print Details':
+          selectedVehicle.printDetails();
+          break;
+        case 'Start':
+          selectedVehicle.start();
+          break;
+        case 'Accelerate':
+          selectedVehicle.accelerate(5);
+          break;
+        case 'Decelerate':
+          selectedVehicle.decelerate(5);
+          break;
+        case 'Stop':
+          selectedVehicle.stop();
+          break;
+        case 'Turn Right':
+          selectedVehicle.turn('right');
+          break;
+        case 'Turn Left':
+          selectedVehicle.turn('left');
+          break;
+        case 'Reverse':
+          selectedVehicle.reverse();
+          break;
+        case 'Wheelie':
+          (selectedVehicle as Motorbike).wheelie();
+          break;
+      }
+  
+      this.selectVehicle();
     }
-}
+  }
